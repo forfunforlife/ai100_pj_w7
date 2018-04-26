fc43fb0396b01125913f3bea8a1c4bca68215967
forfunforlife <yus.liu@hotmail.com>
2018-04-26 17:55:01 +0800

    implemented densenet.py

--- nets/densenet.py
+++ nets/densenet.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 
 slim = tf.contrib.slim
 
+NUM_CLASSES = 100
 
 def trunc_normal(stddev): return tf.truncated_normal_initializer(stddev=stddev)
 
@@ -27,6 +28,19 @@ def block(net, layers, growth, scope='block'):
         tmp = bn_act_conv_drp(bottleneck, growth, [3, 3],
                               scope=scope + '_conv3x3' + str(idx))
         net = tf.concat(axis=3, values=[net, tmp])
+    return net
+
+def block_basic(net, layers, growth, scope='block'):
+    for idx in range(layers):
+        tmp = bn_act_conv_drp(net, growth, [3, 3],
+                              scope=scope + '_conv3x3' + str(idx))
+        net = tf.concat(axis=3, values=[net, tmp])
+    return net
+
+def transition_layer(net, scope='transition'):
+    net = slim.batch_norm(net, scope=scope + '_bn')
+    net = slim.conv2d(net, reduce_dim(net), [1, 1], scope=scope + '_conv')
+    net = slim.ave_pool(net, [2, 2], [2, 2], scope=scope + '_pool')
     return net
 
 
@@ -49,7 +63,7 @@ def densenet(images, num_classes=1001, is_training=False,
       end_points: a dictionary from components of the network to the corresponding
         activation.
     """
-    growth = 24
+    growth = 12
     compression_rate = 0.5
 
     def reduce_dim(input_feature):
@@ -64,6 +78,29 @@ def densenet(images, num_classes=1001, is_training=False,
             ##########################
             # Put your code here.
             ##########################
+            
+            #image size after preprocessing is [32, 32]
+            net = slim.batch_norm(images, scope=scope + 'init_bn')
+            net = tf.nn.relu(net)
+            net = slim.conv2d(images, 16, [1, 1], scope='init_conv')
+            
+            net = block_basic(net, 32, growth, scope='block1')            
+            net = transition_layer(net, scope='transition1')
+            
+            net = block_basic(net, 32, growth, scope='block2')
+            net = transition_layer(net, scope='transition2')
+            
+            net = block_basic(net, 32, growth, scope='block2')
+            net = transition_layer(net, scope='transition3')
+            
+            net = slim.ave_pool(net, [8, 8], [1, 1], scope='global_pool')
+            
+            end_points['GlobalPool'] = net
+          
+            logits = slim.fully_connected(net, NUM_CLASSES, activation_fn=None,
+                                        scope='Logits')
+            end_points['Logits'] = logits
+            end_points['Predictions'] = tf.nn.softmax(logits, name='Predictions')
 
     return logits, end_points
 
